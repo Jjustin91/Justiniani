c10b937ccfb4701a7a794236a8e5fcafdcd94940
Jjustin91 <justinianijonathan91@gmail.com>
2026-02-26 23:54:53 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -1,9 +1,11 @@
+import Profilephoto from '../assets/images/profile.jpg';
+
 function About() {
     return (
         <div className="container">
             <section className="about">
                 <h2>About Me</h2>
-                <img src="src/assets/images/profile.jpg" alt="Profile photo" width="300" height="300"></img>
+                <img src={Profilephoto} alt="Profile photo" width="300" height="300"></img>
                 <p>Hi I'm Jonathan From IT2R3</p>
                 <p>Email:
                     <a href="mailto:justinianijonathan91@gmail.com">justinianijonathan91@gmail.com</a><br />
